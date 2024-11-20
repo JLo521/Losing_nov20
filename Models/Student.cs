d28b95159692dbf87503bad0134cce2d401b221b
Jack Losing <jack.losing@ndus.edu>
2024-11-20 14:44:13 -0600

    New Class - Student.cs

---
+++ Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace Losing_nov20_cl2.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastDescription { get; set; }
+        
+    }
+}
